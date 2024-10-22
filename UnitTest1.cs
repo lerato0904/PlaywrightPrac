85b8bf0420f99db3e77a03db9f106211b83acb95
radebelerato@gmail.com <radebelerato@gmail.com>
2024-10-22 22:04:52 +0200

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PlaywrightPrac
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
