e0e5358fd48b5b3cb57945035ed75d4c261445ce
radebelerato@gmail.com <radebelerato@gmail.com>
2024-11-18 23:07:40 +0200

    update tests to run on multiple browsers

--- PlaywrightTests.cs
+++ PlaywrightTests.cs
@@ -1,62 +1,104 @@
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
-using NUnit.Framework;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using Bogus;
+using Bogus.DataSets;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PlaywrightPrac
 {
-    [Parallelizable(ParallelScope.Self)]
     [TestFixture]
     public class PlaywrightTests : PageTest
     {
-        //// Override the ContextOptions method to configure browser context
-        //protected override async Task<IBrowserContext> NewBrowserContextAsync()
-        //{
-        //    var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        //    {
-        //        Headless = false // Disable headless mode
-        //    });
+        Faker faker;
 
-        //    return await browser.NewContextAsync();
-        //}
+        public PlaywrightTests()
+        {
+            faker = new Faker();
+        }
 
         [Test]
-        public async Task GetStartedLink()
+        [TestCase("chromium")]
+        [TestCase("firefox")]
+        public async Task NavigateToLandingPage(string browserType)
         {
+            // Launch the specified browser
+            // Set to true for headless mode
+            //When set to True: Launches the browser in headless mode, meaning the browser runs in the background without a UI.
+            //This is common for CI/CD pipelines and automation environments
+            IBrowser browser = browserType switch
+            {
+                "chromium" => await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false }),
+                "firefox" => await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false }),
+                "webkit" => await Playwright.Webkit.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false }),
+                _ => throw new ArgumentException($"Unsupported browser: {browserType}")
+            };
+
+
+
+            // Create an isolated session within the browser - Useful for running parallel tests or scenarios without interference.
+            var context = await browser.NewContextAsync();
+
+            // Creates a new tab (or page) within the browser context.
+            // This page can be used to interact with the web application, including navigation, input simulation, and assertions.
+            var page = await context.NewPageAsync();
+
             // Navigate to the page
-            await Page.GotoAsync("https://automationexercise.com");
+            await page.GotoAsync("https://automationexercise.com");
 
             // Verify the page title
-            await Expect(Page).ToHaveTitleAsync(new Regex("Automation Exercise"));
+            await Expect(page).ToHaveTitleAsync(new Regex("Automation Exercise"));
+
+            //Close broser
+            await browser.CloseAsync();
         }
 
         [Test]
         public async Task SignUpNewUser()
         {
+            string name = faker.Name.FirstName();
+            string email = faker.Name.FirstName() + "@test.com";
+
+            // Launch Chrome browser
+            var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false // Set to true for headless mode
+            });
+
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+
             // Navigate to the page
-            await Page.GotoAsync("https://automationexercise.com");
+            await page.GotoAsync("https://automationexercise.com");
 
             // Verify the page title
-            await Expect(Page).ToHaveTitleAsync(new Regex("Automation Exercise"));
+            await Expect(page).ToHaveTitleAsync(new Regex("Automation Exercise"));
 
             // Click on the "Signup/Login" link
-            await Page.Locator("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a").ClickAsync();
+            await page.Locator("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a").ClickAsync();
 
             // Wait for the new page to load
-            await Page.WaitForLoadStateAsync(LoadState.Load);
+            await page.WaitForLoadStateAsync(LoadState.Load);
 
             // Enter name in the signup form
-            await Page.Locator("#form > div > div > div:nth-child(3) > div > form > input[type=text]:nth-child(2)").FillAsync("randy");
+            await page.Locator("//*[@id=\"form\"]/div/div/div[3]/div/form/input[2]").FillAsync(name);
+
+
+            // Enter email in the signup form
+            await page.Locator("//*[@id=\"form\"]/div/div/div[3]/div/form/input[3]").FillAsync(email);
+
 
             // Click the "Signup" button
-            await Page.Locator("#form > div > div > div:nth-child(3) > div > form > button").ClickAsync();
+            await page.Locator("//*[@id=\"form\"]/div/div/div[3]/div/form/button").ClickAsync();
 
             // Wait for the new page to load
-            await Page.WaitForLoadStateAsync(LoadState.Load);
+            await page.WaitForLoadStateAsync(LoadState.Load);
+
+            await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded); // Or DOMContentLoaded
 
             // Assert that the new page contains the text "ENTER ACCOUNT INFORMATION"
-            await Expect(Page.Locator("#form > div > div > div > div.login-form > h2 > b")).ToHaveTextAsync("ENTER ACCOUNT INFORMATION");
+            await Expect(page.Locator("//*[@id=\"form\"]/div/div/div/div[1]/h2/b")).ToHaveTextAsync("Enter Account Information");
+
         }
     }
 }
